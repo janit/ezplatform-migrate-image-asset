a38fc81c3cecd6129590d6cd15591a5024842555
Adam Wójs <adam@wojs.pl>
2020-07-11 12:10:58 +0200

    Fixed code style issues

--- src/MigrateEzImageToAssetBundle.php
+++ src/MigrateEzImageToAssetBundle.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Janit\MigrateEzImageToAssetBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MigrateEzImageToAssetBundle extends Bundle {
-
+final class MigrateEzImageToAssetBundle extends Bundle
+{
 }
